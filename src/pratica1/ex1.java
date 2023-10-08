dc61bc39668b6adafe9ac5fa05ad7ae9e5070927
Gabriel Loureiro <loureirogabrieloficial@gmail.com>
2023-10-08 17:46:16 -0300

    Exercício 1

--- src/pratica1/ex1.java
+++ src/pratica1/ex1.java
@@ -1,7 +1,23 @@
 package pratica1;
 
+import java.util.Scanner;
+
 public class ex1 {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.print("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("A média das notas é: " + media);
     }
 }
--- src/pratica1/ex6.java
+++ src/pratica1/ex6.java
@@ -1,4 +1,5 @@
 package pratica1;
 
 public class ex6 {
+
 }
